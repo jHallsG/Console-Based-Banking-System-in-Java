e9b75a2d6cfabce1865e0e2438b3fa3670acde87
Jan Hassan Gahal <janhalls202gahal@gmail.com>
2024-02-24 13:35:13 +0800

    did some refactor

--- src/com/JavaATM/main/LoginDisplay.java
+++ src/com/JavaATM/main/LoginDisplay.java
@@ -12,16 +12,10 @@ public class LoginDisplay implements ConsoleDisplays{
 	@Override
 	public void show() {
 		System.out.print("\nPlease enter your email address: \n>> ");
-		//scan.nextLine();	//dont't delete. somehow scanner detects the newline \n as an input
-			
 		String username = scan.nextLine();
 			
 		System.out.print("\nPlease enter your password: \n>> ");
 		String pass = scan.nextLine();
-		
-		System.out.println("Username: " + username);
-		System.out.println("Password: " + pass);
-			
 	}
 	
 	public boolean authenticateSuccessfulLogin() {
--- src/com/JavaATM/main/Main.java
+++ src/com/JavaATM/main/Main.java
@@ -2,6 +2,7 @@ package com.JavaATM.main;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
 public class Main {
 
@@ -18,5 +19,6 @@ public class Main {
 //        for (String beanName : beanNames) {
 //            System.out.println(beanName);
 //        }
+		((AnnotationConfigApplicationContext) context).close();
 	}
 }
